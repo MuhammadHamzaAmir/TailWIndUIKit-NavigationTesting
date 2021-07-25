52899f4aa6b74a36fbaddd3e9f26382adb69b4c3
Muhammad Hamza Amir <muhammadhamza2084@gmail.com>
2021-07-25 22:00:44 +0500

    change of comments

--- test/cookies/cookies_load.js
+++ test/cookies/cookies_load.js
@@ -7,18 +7,16 @@ async function cookies_load(page){
 const previousSession = fs.existsSync(cookiesFilePath);
 if (previousSession) {
   // If file exist load the cookies
-  const cookiesString = fs.readFileSync(cookiesFilePath,'utf-8');
+  const cookiesString = fs.readFileSync(cookiesFilePath);
   const parsedCookies = JSON.parse(cookiesString);
-  await page.setCookie(parsedCookies);
-  console.log("Session has been loaded in the browser");
-  /*
+
   if (parsedCookies.length !== 0) {
     for (let cookie of parsedCookies) {
-      await page.setCookie.apply(page,cookie);
+      await page.setCookie(cookie);
       console.log('Session has been loaded in the browser iiiii');
     }
     console.log('Session has been loaded in the browser');
-  }*/
+  }
     }
 };
 
