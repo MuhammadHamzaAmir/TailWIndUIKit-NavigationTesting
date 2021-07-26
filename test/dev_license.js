30699b924672c9bdad87279cff188ed80b466301
Muhammad Hamza Amir <muhammadhamza2084@gmail.com>
2021-07-26 19:05:56 +0500

    dev_license new page gets active

--- test/dev_license.js
+++ test/dev_license.js
@@ -21,15 +21,14 @@ const puppeteer = require("puppeteer");
 
   await page.goto("https://tailwinduikit.com/pricing"); //mentioned site is then reached
   await page.waitForTimeout(2000); // delay for 5 second for website to load
-  await dev_lisence(page);
-
+  await dev_lisence(browser,page);
   await page.waitForTimeout(5000); // delay for 5 second for website to load
 
   await browser.close();
 })();
 
 //function for div license
-async function dev_lisence(page_entry){
+async function dev_lisence(browser,page_entry){
 
     var xpath_div_lis_button = "//*[@id='p2']";  
     await page_entry.waitForTimeout(3000); // delay of 3 seconds
@@ -45,8 +44,20 @@ async function dev_lisence(page_entry){
         })
     ); //scrolling till that component
 
+  //const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page())));  // declare promise for new page
+
+
   await page_entry.waitForTimeout(4001); // delay of 4 seconds
-  await div_lis_clk.click(); //jointuk button is clicked
+  await div_lis_clk.click({button:"middle"}); //dev-license button is clicked
+  await page_entry.waitForTimeout(4001); // delay of 4 seconds
+    let pages_l = await browser.pages();
+    console.log(pages_l.length);
+    var page_2 = pages_l[2];
+    page_2.bringToFront();
+  //console.log(newPagePromise);
+  //const new_page = await newPagePromise;    // declaring the new tab
+//console.log(new_page);
+ // await new_page.bringToFront();        //make the new tab active
     
 
 }
