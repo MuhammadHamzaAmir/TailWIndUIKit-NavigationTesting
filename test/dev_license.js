3a1105391a77f37d67236b9416107cbf876688d3
Faiz Ul Hassan <45035217+faiz-ul-hassan747284@users.noreply.github.com>
2021-07-27 14:48:58 +0500

    dev_license test is completed

--- test/dev_license.js
+++ test/dev_license.js
@@ -11,8 +11,6 @@ const puppeteer = require("puppeteer");
     args: ["--start-maximized"],
   }); //browser is launched
 
-  // Create a new incognito browser context.
-  //const context = await browser.createIncognitoBrowserContext(); // for testing
 
   var page = await browser.newPage(); // a new page is created
 
@@ -22,7 +20,7 @@ const puppeteer = require("puppeteer");
   await page.goto("https://tailwinduikit.com/pricing"); //mentioned site is then reached
   await page.waitForTimeout(2000); // delay for 5 second for website to load
   await dev_lisence(browser,page);
-  await page.waitForTimeout(5000); // delay for 5 second for website to load
+  await page.waitForTimeout(4000); // delay for 4 second for website to load
 
   await browser.close();
 })();
@@ -44,20 +42,25 @@ async function dev_lisence(browser,page_entry){
         })
     ); //scrolling till that component
 
-  //const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page())));  // declare promise for new page
-
 
   await page_entry.waitForTimeout(4001); // delay of 4 seconds
   await div_lis_clk.click({button:"middle"}); //dev-license button is clicked
   await page_entry.waitForTimeout(4001); // delay of 4 seconds
-    let pages_l = await browser.pages();
-    console.log(pages_l.length);
-    var page_2 = pages_l[2];
-    page_2.bringToFront();
-  //console.log(newPagePromise);
-  //const new_page = await newPagePromise;    // declaring the new tab
-//console.log(new_page);
- // await new_page.bringToFront();        //make the new tab active
-    
+  
+  let pages_l = await browser.pages();  //getting array of opened pages in the browser
+
+  var page_2 = pages_l[(pages_l.length)-1];   //last page
+  page_2.bringToFront();  //activating the tab
+  
+ 
+ await page_entry.waitForTimeout(5000); // delay of 5 seconds
+ var xpath_of_buy_b = "//*[@id='product_page']/div[2]/div[2]/div[2]/div/div/div[1]/div[4]/div/div[2]/div[1]/form/button";
+ let buy_b = await page_2.waitForXPath(
+  xpath_of_buy_b,
+  { visible: true }
+);
+
+await buy_b.click(); //buy this button is clicked
+await page_2.waitForTimeout(3000); // delay for 3 second for website to load
 
 }
